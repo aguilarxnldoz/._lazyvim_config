7865a2c4dd9c11341b8407f69b1b86979a1bcd9d
hateNaldoz <aguilarnald@gmail.com>
2026-04-05 17:01:46 -0700

    add: cellular automaton

---
+++ lua/plugin/edit/cellular-automaton-plugin.lua
@@ -0,0 +1,16 @@
+return {
+  "eandrju/cellular-automaton.nvim",
+  cmd = "CellularAutomaton",
+  keys = {
+    {
+      "<leader>fml",
+      "<cmd>CellularAutomaton make_it_rain<CR>",
+      desc = "Cellular automaton: make it rain",
+    },
+    {
+      "<leader>fmg",
+      "<cmd>CellularAutomaton game_of_life<CR>",
+      desc = "Cellular automaton: game of life",
+    },
+  },
+}
